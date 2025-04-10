ec75c30b4a0b993727d47028f971fbcc66ebed1e
Safwan S M <chappu230@gmail.com>
2025-04-10 14:26:38 +0530

    commit

---
+++ PentagonSpace-main/class1/add mult sub div.py
@@ -0,0 +1,21 @@
+class Calci:
+    def arithmatic(self,a,b):
+        c1=a+b
+        c2=a-b
+        c3=a*b
+        c4=a/b
+        return c1,c2,c3,c4
+c=Calci()
+x=5
+y=2
+r1,r2,r3,r4=c.arithmatic(x,y)
+print(r1)
+print(r2)
+print(r3)
+print(r4)
+
+
+
+
+
+
